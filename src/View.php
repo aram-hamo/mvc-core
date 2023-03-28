7cafdff884739d7f2c4c2a2299febfa446e1d7c6
Aram Hamo <contact@aramhamo.me>
2023-03-28 10:35:25 +0200

    new file:   .gitignore new file:   composer.json new file:   src/Controller.php new file:   src/DB.php new file:   src/Migration.php new file:   src/Router.php new file:   src/Routes.php new file:   src/Schema.php new file:   src/View.php

---
+++ src/View.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+namespace AramHamo\MvcCore;
+
+class View{
+  public static $viewData = array();
+  public static function render(String $view,Array $viewData){
+    self::$viewData = $viewData;
+    return include __DIR__.'/../views/'.$view.'.php';
+  }
+}
