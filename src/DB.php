480da33fe88e808a1c6fdd243c3d20e5cf23dd29
Aram Hamo <contact@aramhamo.me>
2023-04-03 18:40:28 +0200

    modified:   DB.php

--- src/DB.php
+++ src/DB.php
@@ -7,19 +7,15 @@ class DB {
 
     switch(CONFIG["DATABASE"]["SOFTWARE"]){
       case "mysql":
-        $GLOBALS["dsn"] = "mysql:host=". CONFIG["DATABASE"]["HOST"].";dbname=". CONFIG["DATABASE"]["DATABASE"];
+        $GLOBALS["dsn"] = "mysql:host=". CONFIG["DATABASE"]["HOST"].";dbname=". CONFIG["DATABASE"]["NAME"];
+        $this->conn = new \PDO($GLOBALS["dsn"],CONFIG["DATABASE"]["USERNAME"],CONFIG["DATABASE"]["PASSWORD"]);
+        $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
       break;
       case "sqlite":
         $GLOBALS["dsn"] = "sqlite:/".__DIR__."/../../../../". CONFIG["DATABASE"]["PATH"];
+        $this->conn = new \PDO($GLOBALS["dsn"]);
+        $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
       break;
-    }
-    try{
-      $this->conn = new \PDO($GLOBALS["dsn"]);
-      $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    }catch(Exception $e){
-      if(CONFIG["DEBUG_MODE"]){
-        echo $e;
-      }
     }
   }
 }
