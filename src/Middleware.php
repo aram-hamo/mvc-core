b51b5507841b621846c293788c5ea696ef7f1095
Aram Hamo <contact@aramhamo.me>
2023-04-13 17:27:38 +0200

    modified:   src/Middleware.php

--- src/Middleware.php
+++ src/Middleware.php
@@ -2,13 +2,17 @@
 namespace AramHamo\MvcCore;
 
 class Middleware{
-  public static function middleware(array|string $middlewares){
-    if(gettype($middlewares) == 'array'){
-      foreach($middlewares as $middleware ){
-        require '../middlewares/'.$middleware.'.php';
+  public function middleware(...$middlewares){
+    foreach($middlewares as $middleware){
+      if (file_exists('../middlewares/'.$middleware.'.php') && is_readable('../middlewares/'.$middleware.'.php')) {
+        include '../middlewares/'.$middleware.'.php';
+      }else{
+        if(CONFIG["GENERAL"]["DEBUG_MODE"]){
+          echo "Error: couldn't find middleware at: middlewares/$middleware.php";
+          exit;
+        }
+        exit;
       }
-    }else{
-      require '../middlewares/'.$middlewares.'.php';
     }
   }
 }
